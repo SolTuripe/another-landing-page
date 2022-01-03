7982d9a8328ba3d36dbaf71e7d0053bb49676f32
Sol <solcis.bio@gmail.com>
2022-01-03 16:47:07 +0100

    update js

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,27 +1,25 @@
-const imagenes = document.querySelectorAll('.img-galeria')
-const imagenLight = document.querySelector('.agregar-imagen');
-const contenedorLight = document.querySelector('.imagen-light')
-const hamburguer = document.querySelector('.hamburguer');
+const imagenes = document.querySelectorAll(".img-galeria");
+const imagenLight = document.querySelector(".agregar-imagen");
+const contenedorLight = document.querySelector(".imagen-light");
+const closeLight = document.querySelector('.close');
 
-
-imagenes.forEach(imagen => {
-    imagen.addEventListener('click',()=>{
-        aparecerImagen(imagen.getAttribute('src'));
-    })
+imagenes.forEach((imagen) => {
+  imagen.addEventListener("click", () => {
+    aparecerImagen(imagen.getAttribute("src"));
+  });
 });
 
-contenedorLight.addEventListener('click',(e)=>{
-    if(e.target !== imagenLight){
-        contenedorLight.classList.toggle('show')
-        imagenLight.classList.toggle('showImage')
-        hamburguer.style.opacity = '1';
-    }
-})
+contenedorLight.addEventListener("click", (e) => {
+  if (e.target !== imagenLight) {
+    contenedorLight.classList.toggle("show");
+    imagenLight.classList.toggle("showImage");
+    hamburguer.style.opacity = "1";
+  }
+});
 
-
-const aparecerImagen = (imagen)=>{
-    imagenLight.src = imagen;
-    contenedorLight.classList.toggle('show')
-    imagenLight.classList.toggle('showImage')
-    hamburguer.style.opacity = '0';
-}
+const aparecerImagen = (imagen) => {
+  imagenLight.src = imagen;
+  contenedorLight.classList.toggle("show");
+  imagenLight.classList.toggle("showImage");
+  hamburguer.style.opacity = "0";
+};
